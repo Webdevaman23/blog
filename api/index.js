7829e95b1b10434cc5f18015994203d59f5f2ae3
unknown <aman.ap.23052001@gmail.com>
2024-02-25 11:37:38 +0530

    create signup api but there is a error in importing auth files in index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -14,8 +16,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
+app.use(express.json());
+
+app.use('/api/user' , userRoutes);
+app.use('/api/auth' , authRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
