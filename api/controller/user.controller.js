e05fba595c769e4b43cee22e93b95742fa6dc6af
unknown <aman.ap.23052001@gmail.com>
2024-03-13 23:42:25 +0530

    add delete user functionality to userdashboard

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -60,7 +60,7 @@ export const updateUser = async (req, res, next) => {
 
 // delete user
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
     return errorHandler(403, "You are not allowed to delete this user");
   }
   try {
--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -62,6 +62,7 @@ export default function DashUsers() {
         console.log(error.message);
     }
   };
+  
 
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
