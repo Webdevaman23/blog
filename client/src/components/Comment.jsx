890d74ef80c676b51bb0b9c97d9ab1901f183a32
unknown <aman.ap.23052001@gmail.com>
2024-03-19 23:56:03 +0530

    add like functionality to the comment component

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,4 +1,4 @@
-// import moment from 'moment';
+import moment from 'moment';
 import { useEffect, useState } from 'react';
 import { FaThumbsUp } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
