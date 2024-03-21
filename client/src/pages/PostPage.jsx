9ddbd7329c37d44f919f19ab71f3b8197943ae28
unknown <aman.ap.23052001@gmail.com>
2024-03-22 00:15:33 +0530

    add recent article section to the postpage

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -86,7 +86,7 @@ export default function PostPage() {
 
       <div className="flex flex-col justify-center items-center mb-5">
         <h1 className="text-xl mt-5">Recent articles</h1>
-        <div className="">
+        <div className="flex flex-wrap gap-5 mt-5">
           {
             recentPosts && 
             recentPosts.map((post) => (
